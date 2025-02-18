e6be2c40a1d80b796015a752bdf96f1ee07e8a89
Pqvqn <pavanarauch@hotmail.com>
2025-02-18 10:46:18 -0500

    Comments

--- custom_widgets.py
+++ custom_widgets.py
@@ -4,19 +4,26 @@ from PySide6.QtWidgets import QLabel
 
 
 class VerticalText(QLabel):
+    """
+    QLabel that draws text vertically oriented, with the beginning of the line on the bottom
+    """
 
     def __init__(self, text):
         super(VerticalText, self).__init__(text)
 
     def paintEvent(self, e):
+        # Use painter to rotate text
         painter = QPainter(self)
         painter.rotate(270)
+        # Must adjust start position to draw in correct place
         painter.drawText(QPoint(-self.height(), self.width()), self.text())
 
     def sizeHint(self):
+        # Flip sizes to account for rotation
         s = super().sizeHint()
         return QSize(s.height(), s.width())
 
     def minimumSizeHint(self):
+        # Flip sizes to account for rotation
         s = super().minimumSizeHint()
         return QSize(s.height(), s.width())
